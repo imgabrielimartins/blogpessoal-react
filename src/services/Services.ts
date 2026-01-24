4392602172f0071138154aabcfc933edb4665a79
Gabrieli <gabrielidelimamartins@gmail.com>
2026-01-23 22:22:11 -0300

    deploy

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://blogpessoal-nest-lo31.onrender.com/api",
-  headers: { "Content-Type": "application/json" },
+export const api = axios.create({
+ baseURL: import.meta.env.VITE_API_URL
 });
 
 
